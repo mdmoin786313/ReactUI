8982f215784ed81089343dae8bd87330b83de33f
mdmoin786313 <40501016+mdmoin786313@users.noreply.github.com>
2019-02-22 23:48:39 +0530

    FC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 import Header from './components/headerComponent/Header.js';
+import Backg1Component from './components/backg1Component/Backg1Component.js';
 import './Assets/scss/new.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Header />
+        <Backg1Component />
       </div>
     );
   }
