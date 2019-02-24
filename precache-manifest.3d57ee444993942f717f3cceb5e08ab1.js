57a7f7431c0254cd68eb8e895829dc58782d1056
mdmoin786313 <40501016+mdmoin786313@users.noreply.github.com>
2019-02-24 15:16:42 +0530

    Updates

---
+++ precache-manifest.3d57ee444993942f717f3cceb5e08ab1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "28887212d5790f99fe9e",
+    "url": "/ReactUI/static/js/runtime~main.28887212.js"
+  },
+  {
+    "revision": "bbf1f6c7b23aa9a43383",
+    "url": "/ReactUI/static/js/main.bbf1f6c7.chunk.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/ReactUI/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "bbf1f6c7b23aa9a43383",
+    "url": "/ReactUI/static/css/main.23c128b3.chunk.css"
+  },
+  {
+    "revision": "9d08045325870ca303bf55a3bc6a3b07",
+    "url": "/ReactUI/index.html"
+  }
+];
